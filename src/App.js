45da94109ddf7793da44e753eff712439e1b5ae5
Rashmi <rashmi.sinha1st@gmail.com>
2022-10-26 09:07:03 +0530

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+const [count, setCount]=useState(0);
+
+function countIncHandle(){
+  setCount(count+1);
+}
+function countDecHandle(){
+  if(count>0)
+  setCount(count-1);
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <button onClick={countIncHandle}>+</button>
+  <label>{count}</label>
+  <button onClick={countDecHandle}>-</button>
     </div>
   );
 }
